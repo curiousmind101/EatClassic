88ecdefe25e0d3f6a2669a8e75564e850b08d6e4
Ajit Kumar <101949618+curiousmind101@users.noreply.github.com>
2023-08-11 18:31:34 +0530

    updata anchor tag

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -5,7 +5,7 @@ const Signup = ()=>{
         <div>
            <section className="bg-gray-50">
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                    <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900">
+                    <a href="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900">
                         Eat Classic   
                     </a>
                     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
@@ -31,12 +31,12 @@ const Signup = ()=>{
                                       <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" required=""/>
                                     </div>
                                     <div className="ml-3 text-sm">
-                                      <label for="terms" className="font-light text-gray-500">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
+                                      <label for="terms" className="font-light text-gray-500">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="/">Terms and Conditions</a></label>
                                     </div>
                                 </div>
                                 <button type="submit" className="w-full text-white  bg-orange-400 hover:bg-orange-600 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create an account</button>
                                 <p className="text-sm font-light text-gray-500">
-                                    Already have an account? <a href="#" className="font-medium text-primary-600 hover:underline ">Login here</a>
+                                    Already have an account? <a href="/" className="font-medium text-primary-600 hover:underline ">Login here</a>
                                 </p>
                             </form>
                         </div>
