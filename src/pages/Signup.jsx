d1dbe4726c240e43808dcb5aae648db1aeb036bd
Ajit Kumar <101949618+curiousmind101@users.noreply.github.com>
2023-08-11 22:03:53 +0530

    update emailbar

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -15,7 +15,7 @@ const Signup = ()=>{
                             </h1>
                             <form className="space-y-4 md:space-y-6" action="#">
                                 <div>
-                                    <label for="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+                                    <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                                     <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5-blue-500" placeholder="name@company.com" required=""/>
                                 </div>
                                 <div>
