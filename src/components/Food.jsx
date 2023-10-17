4fec71d303b2435b719dc526d1589be844dcd5f2
Ajit Kumar <101949618+curiousmind101@users.noreply.github.com>
2023-10-17 21:21:04 +0530

    add limit to redering

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -17,6 +17,7 @@ const Food = ()=>{
           })
         );
     };
+    const [Limit, setLimit] = useState(8);
     return (
             <div className="max-w-[1620px] m-auto px-4 py-12">
                <h1 className="text-orange-600 font-bold text-4xl text-center">Top Rated Menu Items</h1>
@@ -26,12 +27,12 @@ const Food = ()=>{
                        {/**Filter type */}
                        <p className="font-bold text-gray-700">Filter Type</p>
                        <div className="flex justify-between flex-wrap gap-2">
-                         <button onClick={()=>setFoods(data)} className="border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">All</button>
-                         <button onClick={()=>filterType("paneer")} className="border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">Paneer</button>
-                         <button onClick={()=>filterType("Chicken")} className="border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">Chicken</button>
-                         <button onClick={()=>filterType("biryani")} className="border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">Biryani</button>
-                         <button onClick={()=>filterType("bread")} className="border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">Bread</button>
-                         <button onClick={()=>filterType("sweets")} className="border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">Sweets</button>
+                         <button onClick={()=>{setFoods(data); setLimit(100)}} className="border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">All</button>
+                         <button onClick={()=>{filterType("paneer"); setLimit(100)}} className="border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">Paneer</button>
+                         <button onClick={()=>{filterType("Chicken"); setLimit(100)}} className="border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">Chicken</button>
+                         <button onClick={()=>{filterType("biryani"); setLimit(100)}} className="border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">Biryani</button>
+                         <button onClick={()=>{filterType("bread"); setLimit(100)}} className="border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">Bread</button>
+                         <button onClick={()=>{filterType("sweets"); setLimit(100)}} className="border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">Sweets</button>
                        </div>
                     </div>
                     <div>
@@ -46,7 +47,7 @@ const Food = ()=>{
                     </div>
                </div>
                <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4">
-                {foods.map((item, index)=>(
+                {foods.slice(0, Limit).map((item, index)=>(
                     <div key={index} className="border shadow-lg rounded-lg hover:scale-105 duration-300 rounded-t-lg">
                         <img src={item.image} alt={item.name}
                             className="w-full h-[200px] object-cover rounded-t-lg"
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,11 +3,14 @@ import React from "react";
 
 const Cart = () =>{
     return (
-        <div>
-            <div className="text-center flex justify-center items-center mt-80">
-               <h1 className="text-gray-700 text-3xl bg-white">Your cart is empty</h1>
+        <div  className="items-center flex-col gap-4">
+            <div className="text-center flex justify-center items-center mt-[25vh]">
+               <h1 className="text-gray-600 text-3xl bg-white">your cart is empty....</h1>
                {/* <BsFillCartFill size={100} className="text-center ml-80"/> */}
             </div>
+            <img className=" container mx-auto h-[50vh] w-[70vh] drop-shadow-lg" src="https://cdni.iconscout.com/illustration/premium/thumb/empty-cart-7236766-5875081.png?f=webp" alt="empty cart"/>
+            <p className="justify-center text-center text-gray-600 text-sm bg-white container mx-auto">you haven't added anything yet, please add something to checkout..</p>
+            <hr className="bg-gray-500 mt-14"/>
         </div>
     );
 }
